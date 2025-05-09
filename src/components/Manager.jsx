0e85726ce88c38554237f7335a694f88e5240c9b
Surinder Singh <surindersinghpro@proton.me>
2025-05-09 16:23:23 +0530

    first commit

---
+++ src/components/Manager.jsx
@@ -0,0 +1,88 @@
+import React, { useRef } from 'react'
+
+const Manager = () => {
+   const ref= useRef()
+    const handleEyePassword = () => {
+      // alert("show pass")
+      console.log(ref.current)
+      ref.current.src = ref.current.src === "X" ? "Y" : "X"
+      console.log(ref.current.src)
+
+
+    }
+  return (
+    <div className='container mx-auto mt-10 w-full'>
+      <form className=" w-[50%] mx-auto">
+  <div className="mb-5">
+   
+  <label htmlFor="url" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+        Enter URL
+      </label>
+      <input
+        type="url"
+        id="url"
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        placeholder="https://example.com"
+        required
+      />
+
+
+  </div>
+
+  <div className="mb-5">
+  <div className="flex gap-4">
+    <div className="w-1/2">
+  
+
+<label htmlFor="username" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+      Username
+    </label>
+    <input
+      type="text"
+      id="username"
+      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+      placeholder="Your username"
+      required
+    />
+    </div>
+    <div className="w-1/2">
+      <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+        Password
+      </label>
+      <div className="relative">
+
+      <input
+        type="password"
+        id="password"
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        placeholder="Password"
+        required
+        />
+        <span onClick={handleEyePassword}  className="absolute top-0 right-0 mt-2 mr-3 cursor-pointer">
+            <img ref={ref} src="../icons/eye.svg" alt="eye" />
+            {/* <p ref={ref}>X</p> */}
+        </span>
+        </div>
+
+    </div>
+  </div>
+</div>
+
+
+
+
+  
+
+  <button
+    type="submit"
+    className="m-auto flex px-15 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+  >
+    Save
+  </button>
+</form>
+
+    </div>
+  )
+}
+
+export default Manager
